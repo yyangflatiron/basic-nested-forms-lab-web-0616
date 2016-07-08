39dc854337163b59f7d9829ead8ad1b2205e06fd
yyangflatiron <yichen.yang@flatironschool.com>
2016-07-08 16:28:03 -0400

    Done.

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,7 @@
 class Ingredient < ActiveRecord::Base
   belongs_to :recipe
+  def recipe_attributes=(recipe)
+  	self.recipe=Recipe.find_or_create_by(name:recipe.name)
+  	self.recipe.update(recipe)
+  end
 end
